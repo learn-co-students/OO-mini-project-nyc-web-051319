816121e0fd77078e41867df0d591c451110f8652
github username <github email address>
2019-05-20 14:30:58 -0400

    @chrisyuaners

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,20 @@
+class Ingredient
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.most_common_allergen
+    ingredients = []
+    Allergy.all.each do |allergy|
+      ingredients << allergy.ingredient
+    end
+    freq = ingredients.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+    ingredients.max_by { |v| freq[v] }
+  end
+end
